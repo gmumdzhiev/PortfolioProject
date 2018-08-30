0eac798e1befba1ee6599389811b35dd5c65cb63
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-08-30 15:48:30 +0200

    Little underline animation on the buttons .

--- portfolio-app/src/components/Navigation.js
+++ portfolio-app/src/components/Navigation.js
@@ -5,13 +5,13 @@ import logo from './img/logo.svg'
 const Navigation = () => (
   <nav>
     <img id='logo' src={logo} />
-    <Link to="/">&#9673; HOME</Link>
-    {' '}
     <Link to="/about">&#9673; ABOUT</Link>
+    {' '}
+    <Link to="/contact">&#9673; CONTACT</Link>
     {' '}
     <Link to="/Portfolio">&#9673; PORTFOLIO</Link>
     {' '}
-    <Link to="/contact">&#9673; CONTACT</Link>
+    <Link to="/">&#9673; HOME</Link>
     {' '}
 
   </nav>
