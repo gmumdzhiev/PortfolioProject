6a8407cde949687bd3b6456ccd1b17bb116d4a64
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-08-30 15:27:10 +0200

    Adding a logo in Nav and fitting the rest of the app.

--- portfolio-app/src/components/Navigation.js
+++ portfolio-app/src/components/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Link from './Link'
-
+import logo from './img/logo.svg'
 
 const Navigation = () => (
   <nav>
+    <img id='logo' src={logo} />
     <Link to="/">&#9673; HOME</Link>
     {' '}
     <Link to="/about">&#9673; ABOUT</Link>
